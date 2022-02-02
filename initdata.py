6458265c80aea9db4e844f91ab79f03de78d80cc
muromtsev <t4k.org@mail.ru>
2022-02-02 22:04:40 +0300

    text files

---
+++ initdata.py
@@ -0,0 +1,16 @@
+# инициализация данных для последующего сохранения в файлах
+
+# записи
+bob = {'name': 'Bob Smith', 'age': 42, 'pay': 30000, 'job': 'dev'}
+sue = {'name': 'Sue Jones', 'age': 45, 'pay': 40000, 'job': 'hdw'}
+tom = {'name': 'Tom', 'age': 50, 'pay': 0, 'job': 'None'}
+
+# база данных
+db = {}
+db['bob'] = bob
+db['sue'] = sue
+db['tom'] = tom
+
+if __name__ == '__main__': # если запускается как сценарий, а не как модуль!!!
+    for key in db:
+        print(key, '=>\n ' , db[key])
