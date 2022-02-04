8ebe27cc04b57f9aa4b273841d989c58d0a099fc
muromtsev <t4k.org@mail.ru>
2022-02-04 21:37:39 +0300

    interactive updates

---
+++ peopleinteract_update.py
@@ -0,0 +1,26 @@
+# интерактивные изменения
+import shelve
+from person_start import Person
+
+fieldnames = ('name', 'age', 'job', 'pay')
+db = shelve.open('class-shelve')
+
+while True:
+    key = input('\nKey? => ')
+
+    if not key: break
+    
+    if key in db:
+        record = db[key]
+    else:
+        record = Person(name='?', age='?')
+    
+    for field in fieldnames:
+        currval = getattr(record, field)
+        newtext = input('\t[%s]=%s\n\t\tnew?=>' % (field, currval))
+        if newtext:
+            setattr(record, field, eval(newtext))
+
+    db[key] = record
+
+db.close()
