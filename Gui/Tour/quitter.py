ba1518e8771fe410885ee2d249ab999e5880c987
muromtsev <t4k.org@mail.ru>
2022-02-16 21:25:51 +0300

    create class button Quitter

---
+++ Gui/Tour/quitter.py
@@ -0,0 +1,21 @@
+"""
+кнопка Quit, которая запрашивает подтверждение на завершение
+для повторного использования достаточно прикрепить экземпляр к другому
+графическому интерфейсу и скомпоновать с желаемыми параметрами
+"""
+
+from tkinter import *                       # импорт классов виджетов
+from tkinter.messagebox import askokcancel  # импорт стандартного диалога
+
+class Quitter(Frame):
+    def __init__(self, parent=None):
+        Frame.__init__(self, parent)
+        self.pack()
+        widget = Button(self, text='Quit', command=self.quit)
+        widget.pack(side=LEFT, expand=YES, fill=BOTH)
+
+    def quit(self):
+        ans = askokcancel('Verify exit', 'Really quit?')
+        if ans: Frame.quit(self)
+
+if __name__ == '__main__': Quitter().mainloop()
