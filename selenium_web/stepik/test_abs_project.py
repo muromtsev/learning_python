acec452953b86a8fe5596f1ab727bcf1bac07a44
muromtsev <t4k.org@mail.ru>
2022-06-24 23:42:44 +0300

    lesson 3 tests

--- selenium_web/stepik/test_abs_project.py
+++ selenium_web/stepik/test_abs_project.py
@@ -1,6 +1,16 @@
 def test_abs1():
-    assert abs(-42) == 42, "Should be absolute value of a number" 
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
 
 if __name__ == "__main__":
     test_abs1()
-    print("All tests passed!")
+    test_abs2()
+    print("Everything passed")
+
+
+    """
+    Можно выделить три основных тестовых фреймворка для Python: unittest, PyTest и nose. Модуль unittest является встроенным инструментом Python — и это его большой плюс. PyTest и nose устанавливаются дополнительно, они позволяют получить расширенные возможности по сравнению с unittest. 
+    Еще один плюс использования PyTest в том, что для него существует большое количество плагинов, которые позволяют решить практически любую проблему, связанную с запуском автотестов.
+    """
