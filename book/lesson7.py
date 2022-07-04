ef900ccf8eda97c5ec18e29fe5b3df953812a933
muromtsev <t4k.org@mail.ru>
2022-07-04 09:06:26 +0300

    lesson 7

---
+++ book/lesson7.py
@@ -0,0 +1,15 @@
+# spam = ['cat', 'dog', 'moose']
+# print(id(spam))
+# spam.append('snake')
+# print(id(spam))
+
+spam = {'name': 'Zophie'}
+
+eggs = spam
+
+# print(spam is eggs)
+
+bacon = {'name': 'Zophie'}
+
+# print(spam == bacon)
+print(spam is bacon)
