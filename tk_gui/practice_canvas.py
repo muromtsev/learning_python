3093f29d8ddb644c35f300c441d127e1987c9ce7
muromtsev <t4k.org@mail.ru>
2023-02-18 13:24:12 +0300

    practice canvas

--- tk_gui/practice_canvas.py
+++ tk_gui/practice_canvas.py
@@ -1,18 +1,29 @@
-from  tkinter import *
-
-root = Tk()
-
-c = Canvas(root, width=300, height=200, bg='white')
-c.pack()
-
-ball = c.create_oval(0, 100, 40, 140, fill='green')
-
+from tkinter import Tk, Canvas
+ 
+def click(event):
+  c.x = event.x + c.radius
+  c.y = event.y + c.radius
+  motion()
+ 
 def motion():
-    c.move(ball, 1, 0)
-    
-    if c.coords(ball)[2] < 300:
-        root.after(10, motion)
-motion()
-
-
+  x = c.coords(c.ball)[2]
+  y = c.coords(c.ball)[3]
+  if c.x == x and c.y == y:
+    return
+  if c.x < x:
+    c.move(c.ball,-1, 0)
+  if c.x > x:
+    c.move(c.ball, 1, 0)
+  if c.y < y:
+    c.move(c.ball, 0,-1)
+  if c.y > y:
+    c.move(c.ball, 0, 1)
+  root.after(10, motion)
+ 
+root = Tk()
+c = Canvas(root, width=300, height=200, bg="white")
+c.pack()
+c.radius = 20
+c.ball = c.create_oval(0, 100, c.radius*2, 100+c.radius*2, fill='green')
+c.bind("<Button-1>", click)
 root.mainloop()
