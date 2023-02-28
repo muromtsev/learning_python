58fef240e689b39a223d27d88bdf81550eb79665
muromtsev <t4k.org@mail.ru>
2023-02-28 22:54:52 +0300

    update

--- selenium_web/main.py
+++ selenium_web/main.py
@@ -1,48 +1,36 @@
-from selenium import webdriver
 import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-# options = webdriver.ChromeOptions()
-# options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36")
-# driver = webdriver.Chrome(executable_path='C:\\Users\\ipozv\\OneDrive\\Рабочий стол\\selenium\\chromedirver\\chromedriver.exe', options=options)
-# url = "http://astromebel.ru/"
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+
+url = 'google.com'
+
 # user_agent = 'https://www.whatismybrowser.com/detect/what-is-my-user-agent'
 # Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36
-# try:
-#     driver.get(url)
-    
-#     # email_tag = driver.find_element_by_name('email')
-#     # password_tag = driver.find_element_by_name('password')
-#     # btn_login = driver.find_element_by_id("login-button")
 
-#     # txt = btn_login.text
-#     # email_tag.send_keys('123@123.org')
-#     # password_tag.send_keys('password123')
-#     # btn_login.click()
+try:
+    driver.get(url)
 
-#     stock = []
-#     stock_list = driver.find_element_by_class_name("items_list")
-#     divs = stock_list.find_elements_by_class_name("item_wrap")
+    stock = []
+    stock_list = driver.find_element(By.CLASS_NAME, "items_list")
+    divs = stock_list.find_elements(By.TAG_NAME, "li")
 
-#     for div in divs:
-#         title = div.find_element_by_css_selector('.title a').text
-#         price = div.find_element_by_class_name('price').text
-#         stock.append((title, price))
-        
-    
-#     time.sleep(5)
-    
-#     with open(file='price.txt', mode='w') as file:
-#         for s in range(len(stock)):
-            
-#             file.write(f"{stock[s]}\n")
-# except Exception as ex:
-#     print(ex)
-# finally:
-#     driver.close()
-#     driver.quit()
+    for div in divs:
+        title = div.find_element(By.CSS_SELECTOR, '.title a').text
+        price = div.find_element(By.CLASS_NAME, 'price').text
+        stock.append(f"{title}: {price}")
 
+    time.sleep(3)
 
+    with open(file='price.txt', mode='w', encoding='utf=8') as file:
+        for elem in stock:
+            file.write(f"{elem}\n")
 
-
-
-
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
