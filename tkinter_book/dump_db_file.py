31bc506db7c7e7743dc9a345462a18d114537907
muromtsev <t4k.org@mail.ru>
2024-01-01 23:58:03 +0300

    Delete empty files

--- tkinter_book/dump_db_file.py
+++
@@ -1,7 +0,0 @@
-from make_db_file import loadDbase
-
-# загрузка БД из файла
-db = loadDbase()
-for key in db:
-    print(key, '=>\n ', db[key])
-print(db['sue']['name'])
--- tkinter_book/update_db_file.py
+++
@@ -1,7 +0,0 @@
-from make_db_file import loadDbase, storeDbase
-
-# вносит изменения в БД и сохраняет обратно в файл
-db = loadDbase()
-db['sue']['pay'] *= 1.10
-db['tom']['name'] = 'Tom Tom'
-storeDbase(db)
